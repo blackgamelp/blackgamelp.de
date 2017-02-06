4099c473442176f55a2e892fe2446ee3934c8c41
Steffen Meyer <admin@blackgamelp.de>
2017-02-06 18:01:20 +0100

    updated build

--- webpack.config.js
+++ webpack.config.js
@@ -22,17 +22,17 @@ module.exports = {
     //   template: 'src/index.html',
     //   inject: 'body',
     // }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //     compress: {
-    //         screw_ie8: true,
-    //         warnings: false
-    //     },
-    // }),
-    // new webpack.DefinePlugin({
-    //         'process.env': {
-    //             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    //         }
-    //     }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            screw_ie8: true,
+            warnings: false
+        },
+    }),
+    new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify("production")
+            }
+        }),
   ],
 
   // Transform source code using Babel and React Hot Loader
